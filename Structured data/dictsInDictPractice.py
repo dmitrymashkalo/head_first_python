fe8c4181c938fd2f1a48dde6fb7e7454740c62a3
DmitryMashkalo <dmitrymashkalo@gmail.com>
2023-01-22 20:30:28 +0100

    Simple change

--- Structured data/dictsInDictPractice.py
+++ Structured data/dictsInDictPractice.py
@@ -1,29 +1,30 @@
 import pprint
 
-people = {}
-people['Ford'] = {
-                'Name': 'Ford Perfect',
-                'Gender': 'Male',
-                'Occupation': 'Researcher',
-                'Home Planet': 'Betelgeuse Seven'
-                }
-people['Arthur'] = {
-                'Name': 'Arthur Dent',
-                'Gender': 'Male',
-                'Occupation': 'Sandwich-Maker',
-                'Home Planet': 'Earth'
-                }
-people['Trillian'] = {
-                'Name': 'Tricia McMillan',
-                'Gender': 'Female',
-                'Occupation': 'Mathematician',
-                'Home Planet': 'Earth'
-                }
-people['Robot'] = {
-                'Name': 'Marvin',
-                'Gender': 'Unknow',
-                'Occupation': 'Paranoid Android',
-                'Home Planet': 'Unknow'
-                }
+people = {
+    'Ford': {
+        'Name': 'Ford Perfect',
+        'Gender': 'Male',
+        'Occupation': 'Researcher',
+        'Home Planet': 'Betelgeuse Seven'
+    },
+    'Arthur': {
+        'Name': 'Arthur Dent',
+        'Gender': 'Male',
+        'Occupation': 'Sandwich-Maker',
+        'Home Planet': 'Earth'
+    },
+    'Trillian': {
+        'Name': 'Tricia McMillan',
+        'Gender': 'Female',
+        'Occupation': 'Mathematician',
+        'Home Planet': 'Earth'
+    },
+    'Robot': {
+        'Name': 'Marvin',
+        'Gender': 'Unknow',
+        'Occupation': 'Paranoid Android',
+        'Home Planet': 'Unknow'
+    }
+}
 
 pprint.pprint(people)
