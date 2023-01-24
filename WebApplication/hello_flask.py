81b409732089398f8e6f0ba6dde61cf183d4b300
DmitryMashkalo <dmitrymashkalo@gmail.com>
2023-01-24 15:55:47 +0100

    Final version of webapp

--- WebApplication/hello_flask.py
+++ WebApplication/hello_flask.py
@@ -1,22 +1,27 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 from vsearch import search_for_letters
 
 app = Flask(__name__)
 
 
-@app.route('/')
-def hello() -> str:
-    return 'Hello world from Flask!'
-
-
 @app.route('/search', methods=['POST'])
-def do_search() -> str:
-    return str(search_for_letters('Life, the universe, and everything!', 'eiru,!'))
+def do_search() -> 'html':
+    phrase = request.form['phrase']
+    letters = request.form['letters']
+    title = 'Here are your results:'
+    results = str(search_for_letters(phrase, letters))
 
+    return render_template('results.html',
+                           the_title=title,
+                           the_phrase=phrase,
+                           the_letters=letters,
+                           the_results=results)
 
+@app.route('/')
 @app.route('/entry')
 def entry_page() -> 'html':
-    return render_template('entry.html', the_title='Welcome to search for letters on the web!')
+    return render_template('entry.html',
+                           the_title='Welcome to search for letters on the web!')
 
 
-app.run()
+app.run(debug='True')
