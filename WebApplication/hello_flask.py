39125ef77ca801048bc6a70d803cae1135cccc20
DmitryMashkalo <dmitrymashkalo@gmail.com>
2023-01-24 12:05:35 +0100

    Added do_search fucntion

--- WebApplication/hello_flask.py
+++ WebApplication/hello_flask.py
@@ -1,4 +1,5 @@
 from flask import Flask
+from vsearch import search_for_letters
 
 app = Flask(__name__)
 
@@ -6,6 +7,11 @@ app = Flask(__name__)
 @app.route('/')
 def hello() -> str:
     return 'Hello world from Flask!'
+
+
+@app.route('/search')
+def do_search() -> str:
+    return str(search_for_letters('Life, the universe, and everything!', 'eiru,!'))
 
 
 app.run()
---
+++ WebApplication/vsearch.py
@@ -0,0 +1,12 @@
+def search_for_vowels(phrase: str) -> set:
+    """ Outputs the vowels found in the input word."""
+    vowels = set('aeiou')
+    return vowels.intersection(set(phrase))
+
+
+def search_for_letters(phrase: str, letters: str = 'aeiou') -> set:
+    """ Returns the set of letters from "letters" found in the specified "phrase"."""
+    return set(letters).intersection(set(phrase))
+
+
+print(search_for_letters('hello', 'aeiou'))
