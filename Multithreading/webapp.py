fc132a7f18ac077242a3dc272ced3805574801e1
Dzmitry Mashkala <dzmitry_mashkala@epam.com>
2023-02-14 22:02:04 +0100

    Use threading for write data to database

--- Multithreading/webapp.py
+++ Multithreading/webapp.py
@@ -1,6 +1,8 @@
-from flask import Flask, render_template, request, escape, session
+from flask import Flask, render_template, request, escape, session, copy_current_request_context
 from DBcm import UseDatabase, ConnectionError, CredentialsError, SQLError
 from checker import check_logged_in
+from threading import Thread
+from time import sleep
 
 
 app = Flask(__name__)
@@ -16,28 +18,32 @@ def search_for_letters(phrase: str, letters: str = 'aeiou') -> set:
     return set(letters).intersection(set(phrase))
 
 
-def log_request(req: 'flask_request', res: str) -> 'None':
-    """ Save requests logs in database """
-    try:
-        with UseDatabase(app.config['dbconfig']) as cursor:
-            _SQL = """insert into log (phrase, letters, ip, browser_string, results) values (%s, %s, %s, %s, %s)"""
-            cursor.execute(_SQL, (req.form['phrase'],
-                                req.form['letters'],
-                                req.remote_addr,
-                                req.headers.get('User-Agent'),
-                                res))
-    except Exception as error:
-        print('Somthing went wrong:', str(error))
-
-
 @app.route('/search', methods=['POST'])
 def do_search() -> 'html':
+
+    @copy_current_request_context
+    def log_request(req: 'flask_request', res: str) -> 'None':
+        """ Save requests logs in database """
+        sleep(15)
+        try:
+            with UseDatabase(app.config['dbconfig']) as cursor:
+                _SQL = """insert into log (phrase, letters, ip, browser_string, results) values (%s, %s, %s, %s, %s)"""
+                cursor.execute(_SQL, (req.form['phrase'],
+                                    req.form['letters'],
+                                    req.remote_addr,
+                                    req.headers.get('User-Agent'),
+                                    res))
+        except Exception as err:
+            print('Somthing went wrong:', str(err))
+
     phrase = request.form['phrase']
     letters = request.form['letters']
     title = 'Here are your results:'
     results = str(search_for_letters(phrase, letters))
+
     try:
-        log_request(request, results)
+        t = Thread(target=log_request, args=(request, results))
+        t.start()
     except Exception as error:
         print('Logging failed with this error:', str(error))
 
